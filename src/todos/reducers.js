ce9a610a4a3331b1208c6e4687d30b9494f1f8c9
joshraiborde <joshraiborde@gmail.com>
2021-12-04 17:27:29 -0500

    added switch case for CREATE_TODO & REMOVE_TODO

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,3 +1,16 @@
+import { CREATE_TODO, REMOVE_TODO } from './actions';
+
 export const todos = (state, action) => {
-    
+    const {type, payload} = action;
+
+    switch (type) {
+        case CREATE_TODO: {
+
+        }
+        case REMOVE_TODO: {
+            
+        }
+
+    }
+    return state;
 }
