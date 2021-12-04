62490bed69dda3f84948670a8ce466cbcdd2b5db
joshraiborde <joshraiborde@gmail.com>
2021-12-04 17:34:30 -0500

    added a default action to the switch

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,6 +1,6 @@
 import { CREATE_TODO, REMOVE_TODO } from './actions';
 
-export const todos = (state, action) => {
+export const todos = (state = [], action) => {
     const {type, payload} = action;
 
     switch (type) {
@@ -16,6 +16,7 @@ export const todos = (state, action) => {
             const { text } = payload;
             return state.filter(todo => todo.text != text );
         }
+        default: 
+            return state;
     }
-    return state;
 }
