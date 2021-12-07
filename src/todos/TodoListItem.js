8b6fa127666e7fd48cda308accd96bc1ba90ae5e
joshraiborde <joshraiborde@gmail.com>
2021-12-07 12:35:25 -0500

    troubleshooting complete task button

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import './TodoList.css'
+import './TodoListItem.css';
 
-const TodoListItem = ({ todo, onRemovedPressed }) => (
-    <div className="todo-item-container" >
+const TodoListItem = ({ todo, onRemovePressed }) => (
+    <div className="todo-item-container">
         <h3>{todo.text}</h3>
-        <div className="buttons-container" >
-        <button className="completed-button">Mark As Completed</button>
-        <button 
-            onClick={() => onRemovedPressed(todo.text)}
-            className="remove-button">Remove</button>
+        <div className="buttons-container">
+            <button className="completed-button">Mark As Completed</button>
+            <button
+                onClick={() => onRemovePressed(todo.text)}
+                className="remove-button">Remove</button>
         </div>
     </div>
-)
+);
 
 export default TodoListItem;
