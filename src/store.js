0bf58519b3e3d8b4616799436f200617b4a67610
joshraiborde <joshraiborde@gmail.com>
2021-12-07 18:07:44 -0500

    troubleshooting persistedReducer, it was mispelled

--- src/store.js
+++ src/store.js
@@ -1,9 +1,8 @@
 import { createStore, combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
+import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { todos } from './todos/reducers';
-
 
 const reducers = {
     todos,
@@ -16,6 +15,6 @@ const persistConfig = {
 }
 
 const rootReducer = combineReducers(reducers);
-const persistReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = () => createStore(persistReducer);
+export const configureStore = () => createStore(persistedReducer);
