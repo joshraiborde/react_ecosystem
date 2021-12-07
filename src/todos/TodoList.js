8e996a41f632958761ee93b0b281dc7325c171b0
joshraiborde <joshraiborde@gmail.com>
2021-12-07 08:46:52 -0500

    connected mapStateToProps, mapDispatchToProps

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
+import { removeTodo } from './actions';
 import './TodoList.css'
 
 const TodoList = ({ todos = [] }) => (
@@ -11,4 +13,12 @@ const TodoList = ({ todos = [] }) => (
     </div>
 );
 
-export default TodoList;
+const mapStateToProps = state => ({
+    todos: state.todos,
+})
+
+const mapDispatchToProps = dispatch => ({
+    onRemovePressed: text => dispatch(removeTodo(text)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
