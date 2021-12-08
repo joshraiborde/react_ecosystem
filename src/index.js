ea80005c3c6510cd48108f53155779f2ac666eb7
joshraiborde <joshraiborde@gmail.com>
2021-12-08 11:37:00 -0500

    formatted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {persistStore} from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
