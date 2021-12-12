952de2e9592a22ce7cfc4fe8baf216315ffd8d81
joshraiborde <joshraiborde@gmail.com>
2021-12-12 16:17:37 -0500

    added try and catch to dispatch

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,14 +1,20 @@
 import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from "./actions";
 
 export const loadTodos = () => async (dispatch, getState) => {
-    
-    dispatch(loadTodosInProgress());
-    const response = await fetch('http://localhost:8080/todoos');
-    const todos = await response.json();
+    try {
 
-    dispatch(loadTodosSuccess(todos))
+        dispatch(loadTodosInProgress());
+        const response = await fetch('http://localhost:8080/todoos');
+        const todos = await response.json();
+    
+        dispatch(loadTodosSuccess(todos))
+    } catch (e) {
+
+        dispatch(loadTodosFailure());
+        dispatch(displayAlert(e));
+    }
 }
 
 export const displayAlert = text => () => {
-    alert(`You clicked on: ${text}`);
+    alert(text);
 };
