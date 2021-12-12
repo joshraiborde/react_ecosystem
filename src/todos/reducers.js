09dfc61cfbe6a21c74fa0e3e07a30adae9ab4f9a
joshraiborde <joshraiborde@gmail.com>
2021-12-12 16:45:08 -0500

    added reducer with switch case for LOAD_TODOS_IN_PROGRESS, LOAD_TODOS_SUCCESS, LOAD_TODOS_FAILURE

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -7,6 +7,21 @@ import {
     LOAD_TODOS_FAILURE,
 } from './actions';
 
+export const isLoading = (state = false, action) => {
+    const { type } = action;
+
+    switch (type) {
+        case LOAD_TODOS_IN_PROGRESS:
+            return true;
+
+        case LOAD_TODOS_SUCCESS:
+        case LOAD_TODOS_FAILURE:
+            return false;
+        default:
+            return state;
+    }
+}
+
 export const todos = (state = [], action) => {
     const { type, payload } = action;
 
