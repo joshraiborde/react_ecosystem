f2c3be443a2d298f453f0dc7221b7465d60f8387
joshraiborde <joshraiborde@gmail.com>
2021-12-12 16:15:31 -0500

    imported loadTodosInProgress, loadTodosSuccess, loadTodosFailure actions and dispatch

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,5 +1,12 @@
-export const loadTodos = () => async (dispatch, getState) => {
+import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from "./actions";
 
+export const loadTodos = () => async (dispatch, getState) => {
+    
+    dispatch(loadTodosInProgress());
+    const response = await fetch('http://localhost:8080/todoos');
+    const todos = await response.json();
+
+    dispatch(loadTodosSuccess(todos))
 }
 
 export const displayAlert = text => () => {
