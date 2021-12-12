0f72338c832b4eb41defb53abff16b12bc581ff1
joshraiborde <joshraiborde@gmail.com>
2021-12-12 17:04:16 -0500

    added useEffect hook, imported loadTodos from thunk, dispatched startLoadingTodos

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
+import { loadTodos } from './thunks';
 import { removeTodo, markTodoAsCompleted } from './actions';
 import { displayAlert } from './thunks'
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading}) => {
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, [] );
+
     const loadingMessage = <div>Loading todos...</div>;
 
     const content =  (
@@ -24,11 +29,12 @@ const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading})
 }
 
 const mapStateToProps = state => ({
-    isLoading: state.isLoading;
+    isLoading: state.isLoading,
     todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch => ({
+    startLoadingTodos: () => dispatch(loadTodos()),
     onRemovePressed: text => dispatch(removeTodo(text)),
     onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
     // onDisplayAlertClicked: text => dispatch (displayAlert(text)),
