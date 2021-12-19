f3829ac29060de8f68c74ec5bb6754187df9c591
joshraiborde <joshraiborde@gmail.com>
2021-12-19 17:09:55 -0500

    troubleshooting site not loading

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -1,11 +1,11 @@
-import { createSelector } from 'reselect'
+import { createSelector } from 'reselect';
 
 export const getTodos = state => state.todos.data;
 export const getTodosLoading = state => state.todos.isLoading;
 
 export const getIncompleteTodos = createSelector(
     getTodos,
-    (todos) => todos.filter(todos => !todo.isCompleted),
+    (todos) => todos.filter(todo => !todo.isCompleted),
 );
 
 export const getCompletedTodos = createSelector(
